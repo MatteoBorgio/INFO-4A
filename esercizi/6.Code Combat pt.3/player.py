341cd5e94afb9e838c23f996e09095fb76916054
matte <matteo6.borgio@gmail.com>
2025-10-22 18:06:05 +0200

    aggiunta logica di gioco in player.py e implementate funzionalità di potion.py

--- esercizi/6.Code Combat pt.3/player.py
+++ esercizi/6.Code Combat pt.3/player.py
@@ -25,6 +25,14 @@ class Player:
         self.__buffs = buffs
 
     @property
+    def buffs(self) -> list[tuple[str,int,int]]:
+        return self.__buffs
+    
+    @property
+    def potions(self) -> list[Potion]:
+        return self.__potions
+    
+    @property
     def name(self) -> str:
         return self.__name
 
@@ -122,6 +130,69 @@ class Player:
         enemy.take(self.__damage)
         return self.__damage
 
+    def tick_buffs(self) -> None:
+        active_buffs = []
+        for buff in self.__buffs:
+            new_duration = buff[2] - 1
+            if new_duration > 0:
+                active_buffs.append((buff[0], buff[1], new_duration))
+        self.__buffs = active_buffs
+
+    def use_potion(self, p: Potion) -> dict:
+        if p in self.__potions:
+            result = p.apply_to(self)
+            if "error" in result:
+                return result
+            self.__potions.remove(p)
+            return result
+        else:
+            print("Can't use the potion")
+            return {"error": "potion_not_found"}
+
+    def __find_potion(potions: list[Potion], researched_effect: str) -> Potion | None:
+        for potion in potions:
+            if potion.effect == researched_effect:
+                return potion
+            
+    def should_use_potion(self, enemy: "Player") -> Potion | None:
+        if self.__health / self.__max_health < 0.3:                       # Se il player è in pericolo, cerca una pozione di cura
+            potion = self.__find_potion(self.__potions, "heal")
+            if potion is not None:
+                return potion
+        if enemy.strength > self.__strength + 5:                          # Se il nemico ha una forza molto maggiore, cerca un buff di forza
+            potion = self.__find_potion(self.__potions, "buff_str")
+            if potion is not None:
+                return potion
+        if enemy.dexterity > self.__dexterity + 5:                        # Se il nemico ha una destrezza molto maggiore, cerca un buff di destrezza
+            potion = self.__find_potion(self.__potions, "buff_dex")        
+            if potion is not None:
+                return potion
+        has_str_buff = False                                               # Se il player non ha buff attivi su forza o destrezza, allora
+        has_dex_buff = False                                               # prova a cercare una pozione che li dia
+        for buff in self.__buffs:
+            stat = buff[0]
+            if stat == "str":
+                has_str_buff = True
+            else:
+                has_dex_buff = True
+        if has_str_buff == False and has_dex_buff == True:
+            potion = self.__find_potion(self.__potions, "buff_str")
+            if potion is not None:
+                return potion
+        if has_dex_buff == False and has_str_buff:
+            potion = self.__find_potion(self.__potions, "buff_dex")
+            if potion is not None:
+                return potion
+            
+        if has_dex_buff == False and has_str_buff == False:
+            potion = self.__find_potion(self.__potions, "buff_str")
+            if potion is not None:
+                return potion
+            potion = self.__find_potion(self.__potions, "buff_dex")
+            if potion is not None:
+                return potion
+    
     def __str__(self) -> str:
         return f"{self.__name}: (HP: {self.__health}/{self.__max_health})"
+
 
--- esercizi/6.Code Combat pt.3/potion.py
+++ esercizi/6.Code Combat pt.3/potion.py
@@ -14,5 +14,81 @@ class Potion:
         if duration < 0:
             raise ValueError("Duration must be ")
         self.__duration = duration
+        self.__used = False
+    
+    @property
+    def used(self) -> bool:
+        return self.__used
+    
+    @property
+    def name(self) -> str:
+        return self.__name
 
+    @name.setter
+    def name(self, new_name: str) -> None:
+        if new_name == "":
+            raise ValueError("Name can't be empty")
+        if type(new_name) is not str:
+            raise ValueError("Name must be a string")
+        self.__name = new_name
+
+    @property
+    def effect(self) -> str:
+        return self.__effect
+
+    @effect.setter
+    def effect(self, new_effect: str) -> None:
+        if new_effect not in ["heal", "buff_str", "buff_dex"]:
+            raise ValueError("Effect must be heal, buff_dex or buff_str")
+        self.__effect = new_effect
+
+    @property
+    def amount(self) -> int:
+        return self.__amount
+
+    @amount.setter
+    def amount(self, new_amount: int) -> None:
+        if new_amount < 1:
+            raise ValueError("Amount must be >= 1")
+        self.__amount = new_amount
+
+    @property
+    def duration(self) -> int:
+        return self.__duration
+
+    @duration.setter
+    def duration(self, new_duration: int) -> None:
+        if new_duration < 0:
+            raise ValueError("Duration must be >= 0")
+        self.__duration = new_duration
+
+    def __apply_heal(self, target) -> int | dict:
+        if hasattr(target, "heal") and callable(getattr(target, "heal")):
+            return target.heal(self.__amount)
+        else:
+            return {"error": "unsupported_target"}
         
+    def __apply_buff(self, target) -> int | dict:
+        if hasattr(target, "add_buff") and callable(getattr(target, "add_buff")):
+            stat = self.effect.split("_")[1]
+            return target.add_buff(stat, self.__amount, self.__duration)
+        else:
+            return {"error": "unsupported_target"}
+        
+    def apply_to(self, target) -> dict:
+        if self.__used == True:
+            return {"error": "already_consumed"}
+        if self.__effect == "heal":
+            hp_healed = self.__apply_heal(target)
+            if hp_healed is None:
+                return
+            else:
+                self.__used = True
+                return {"effect": self.__effect, "amount": hp_healed, "duration": self.__duration}
+        else:
+            buff = self.__apply_buff(target)
+            if type(buff) == dict:
+                return buff
+            else:
+                self.__used = True
+                return {"effect": self.__effect, "amount": buff, "duration": self.__duration}
